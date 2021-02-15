c8c7cf3bd6cde51d11bb031e5d4c355c1d97f8d6
Gabriel Duarte <gabrield.developer@gmail.com>
2021-02-15 11:58:24 -0300

    feat(directive): create a structural directive

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { RedDirective } from './directives/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
