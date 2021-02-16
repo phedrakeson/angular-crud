0ed1d6a73c306d16faa43f04867361e68e4906ef
Gabriel Duarte <gabrield.developer@gmail.com>
2021-02-16 18:57:50 -0300

    fix(product): fix product model strict

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -12,7 +12,7 @@ export class ProductCreateComponent implements OnInit {
 
   product: Product = {
     name: '',
-    price: 0
+    price: null
   }
 
   constructor(private productService: ProductService, private router: Router) { 
