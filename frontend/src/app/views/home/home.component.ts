341068ccaa8cc2be73ef073a39d75b2f6c2eb443
Gabriel Duarte <gabrield.developer@gmail.com>
2021-02-21 13:57:11 -0300

    feat(header): create a dynamic title for each app view

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private headerService: HeaderService) { 
+    headerService.headerData = {
+      title: 'Início',
+      icon: 'home',
+      routeUrl: '/'
+    }
+   }
 
   ngOnInit(): void {
   }
+
+
 
 }
